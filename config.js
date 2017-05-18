bf79fe1d93674616c3ef3a30b335c7d7670fdba3
Nikolay Berezovskiy <unikolas@MacBook-Pro-Nikolay.local>
2017-05-18 17:42:15 +0100

    Sample config and icons were added

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  svgRoot: 'icons/svg',
+  originalColor: { name: 'blue', value: '#4990E2'},
+  generateColors: [
+      {name: 'black', value: '#000'},
+      {name: 'white', value: '#fff'}
+  ]
+}
